d22dd5826e4255e88365f5da5cfb202ae98f3ef4
WroomiX <vitesfly@gmail.com>
2024-01-30 20:48:27 +0300

    test

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,6 @@
-let number = Math.floor(Math.random() * 100);
-const num = (n) => n % 2 === 0 ? `${n}: Это четное число` : `${n}: Это нечетное число`;
-console.log(num(number));
+let number1 = Math.floor(Math.random() * 100);
+if(number1 % 2 === 0){
+    console.log(number1 + ': Это чётное число')
+}else{
+    console.log(number1 + ': Это нечётное число')
+}

900bde98cbaaa097abeb8bba6eec94eb8b959762
WroomiX <vitesfly@gmail.com>
2024-02-08 01:42:14 +0300

    tests1

---
+++ lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddressCopy = structuredClone(passportWithAddress);
+passportWithAddressCopy.address.city = 'Bobryisk';
+console.log(passportWithAddress.address.city);
+console.log(passportWithAddressCopy.address.city)

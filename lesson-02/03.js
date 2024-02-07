72cd7029c915de68d97faf30e6c5cacc58cdbdf5
WroomiX <vitesfly@gmail.com>
2024-02-08 00:09:42 +0300

    tests

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -2,7 +2,7 @@ const giveMeRightNumbers = (value1, value2, maximum) => {
     if (value1 === value2 || value1 <= maximum) {
         console.log("Success!");
     } else {
-        console.log('Something wrong!');
+        console.log("Something wrong!");
     }
 }
-giveMeRightNumbers(300, 100, 200)
+giveMeRightNumbers(200, 100, 100)

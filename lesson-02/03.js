3257de48dd3aa0b69bb6e85fc2bfdc8d2cb0fb83
WroomiX <vitesfly@gmail.com>
2024-02-08 17:16:14 +0300

    test 4

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,9 +1,1 @@
-const giveMeRightNumbers = (value1, value2, maximum) => {
-    if (value1 === value2 || value1 <= maximum) {
-        console.log('Success!');
-    } else {
-        console.log('Something wrong!');
-    }
-}
 
-giveMeRightNumbers(250, 100, 200)

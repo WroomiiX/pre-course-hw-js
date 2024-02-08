0214c4d1e63663f829613992f2fa351b3e905a12
WroomiX <vitesfly@gmail.com>
2024-02-08 11:34:45 +0300

    tests1

--- lesson-02/05.js
+++
@@ -1,9 +0,0 @@
-let passport = {
-    name: "Petr",
-    surname: "Petrov",
-};
-
-let passportCopy = {...passport};
-passportCopy.name = 'Ivan';
-console.log(passport.name);
-console.log(passportCopy.name);

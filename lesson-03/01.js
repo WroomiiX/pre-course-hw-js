20c99fd969afc59e09ab6b38838da81657098171
WroomiX <vitesfly@gmail.com>
2024-02-18 17:51:38 +0300

    tests 10

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((item, index) => item === 'черный' || item === 'красный' || item === 'желтый').join('-')
+}
+
+console.log(createColorString());// code
